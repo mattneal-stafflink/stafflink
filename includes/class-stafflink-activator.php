bfb59584dd0db0bcf93526c6da54cf38118f32f1
Matthew Neal <68095856+mattneal-stafflink@users.noreply.github.com>
2022-03-01 16:32:14 +1000

    added base template

---
+++ includes/class-stafflink-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.linkedin.com/in/matthew-neal-1ba40997
+ * @since      1.0.0
+ *
+ * @package    Stafflink
+ * @subpackage Stafflink/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Stafflink
+ * @subpackage Stafflink/includes
+ * @author     Matthew Neal <mattyjneal@gmail.com>
+ */
+class Stafflink_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-stafflink-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.linkedin.com/in/matthew-neal-1ba40997
+ * @since      1.0.0
+ *
+ * @package    Stafflink
+ * @subpackage Stafflink/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Stafflink
+ * @subpackage Stafflink/includes
+ * @author     Matthew Neal <mattyjneal@gmail.com>
+ */
+class Stafflink_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
